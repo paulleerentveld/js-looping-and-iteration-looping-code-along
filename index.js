06deba649174582a4dcd37e69b64309d8d422f3f
paulleerentveld <paul@veldnet.com.au>
2022-03-15 17:30:03 +1000

    add functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+function writeCards(arr,event) {
+    let newArr=[]
+    for (let i =0; i < arr.length; i++) {
+        let name=arr[i];
+        newArr.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+
+}
+
+function countDown(num) {
+    let count = -1;
+    let countDown=num;
+    while (count < num) {
+        console.log(countDown--);
+        count++;
+    }
+
+}
+
+
